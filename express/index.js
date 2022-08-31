23d844ec5e2afbc27a580f7921ae87a47f8c846d
Aitrien <aitriens@gmail.com>
2022-08-31 16:24:18 +1200

    API GET returns individual post

--- express/index.js
+++ express/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const { getTopWords } = require('./utils/tags')
+const fs = require('fs').promises
 const app = express()
-const rootPostDir = './server/assets/posts'
+const rootPostDir = '../assets/posts'
 
 /**
  *  Returns the detail of an individual post in json, formatted as:
@@ -13,7 +14,20 @@ const rootPostDir = './server/assets/posts'
  * }
  */
 app.get('/post/:slug', function (req, res) {
-  // ... fill in your own code ...
+  readPost(req.params.slug + '.md')
+  .then(response => {
+    console.log(response)
+    if (response != null) {
+      res.json({
+      post: {
+        content: response,
+        tags: "To be implemented."
+      }
+    })
+    } else {
+      res.status(404).send("Post not found.")
+    }
+  })
 })
 
 /**
@@ -31,5 +45,15 @@ app.get('/posts', function (req, res) {
 })
 
 app.listen(3000, function () {
-  console.log('Dev app listening on port 3000!')
+  console.log('App listening on port 3000!')
 })
+
+// Reads the markdown content corresponding to the provided title
+async function readPost(title) {
+  try {
+    const postData = await fs.readFile(`${rootPostDir}/${title}`)
+    return postData.toString()
+  } catch (error) {
+    console.error(`${error} - Encountered error reading file.`)
+  }
+}
